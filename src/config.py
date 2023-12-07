d98ba215e5c8bc956dd0a16d5324ea62b6cd5956
reubenmiller <reuben.d.miller@gmail.com>
2023-12-07 15:40:10 +0100

    change default gateway

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ WIFI_SSID = ""
 WIFI_PASSWORD = ""
 
 # Enter the local thin-edge.io MQTT broker address
-TEDGE_BROKER_HOST = "rpi5-d83addab8e9f.local"
+TEDGE_BROKER_HOST = "rpi5-d83add9f145a.local"
 TEDGE_BROKER_PORT = 1883
